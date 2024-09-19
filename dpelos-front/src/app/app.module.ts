4c73011693fad3e8278fbc9d31f36f7286f40f5b
jeMartine <jorgeesteban464@gmail.com>
2024-09-19 00:37:49 -0500

    Componente editar

--- dpelos-front/src/app/app.module.ts
+++ dpelos-front/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ListaMascotasComponent } from './empleado/mascota/lista-mascotas/lista-
 import { ActualizarDuenoComponent } from './empleado/dueno/actualizar-dueno/actualizar-dueno.component';
 import { FormsModule } from '@angular/forms';
 import { CrearMascotaComponent } from './empleado/mascota/crear-mascota/crear-mascota.component';
+import { ActualizarMascotaComponent } from './empleado/mascota/actualizar-mascota/actualizar-mascota.component';
 
 
 
@@ -44,7 +45,8 @@ import { CrearMascotaComponent } from './empleado/mascota/crear-mascota/crear-ma
     ListaDuenosComponent,
     ListaMascotasComponent,
     ActualizarDuenoComponent,
-    CrearMascotaComponent
+    CrearMascotaComponent,
+    ActualizarMascotaComponent
   ],
   imports: [
     BrowserModule,
